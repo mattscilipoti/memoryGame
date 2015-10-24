db5d185c4f02cb2ad933b3b48b23ab34c9a54d04
alea q <aleaq@aleas-AppleSmoothie.local>
2015-10-24 16:23:41 -0400

    added card flip functionality

--- js.js
+++ js.js
@@ -1,4 +1,27 @@
 $( document ).ready(function() {
-  
-    // alert("js is working ")
+  $(".cards").click(function(event){
+      if (checkCounter()==false) {
+      // alert("turn over")
+    $("td").click(function(event){
+      $(this).css("background", "red")
+      event.preventDefault() /* blue is default*/
+    })
+  }
 });
+});
+
+var turnsTaken = 0;
+
+// $(".cards").click(function(event){
+//     if (checkCounter()==false) {
+//     alert("turn over")
+//     }
+// });
+
+function checkCounter(){
+  if(turnsTaken >= 2){
+    return false;
+  }
+  turnsTaken++;
+  return true;
+}
