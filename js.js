20497c97770196c1823ab0998b5f042f35617c2d
alea q <aleaq@aleas-AppleSmoothie.local>
2015-10-24 17:12:52 -0400

    put in setTimeout to switch cards after switch

--- js.js
+++ js.js
@@ -1,15 +1,22 @@
-$( document ).ready(function() {
-  $(".cards").click(function(event){
-      if (checkCounter()==false) {
-      // alert("turn over")
-    $("td").click(function(event){
-      $(this).css("background", "red")
-      event.preventDefault() /* blue is default*/
-    })
-  }
-});
-});
+$(document).ready(function() {
+  $("td").click(function(event) {
+      if (checkCounter() == false) { /*stop from clicking more than two*/
+        $(this).css("background", "red")
+        // event.preventDefault() /* blue is default*/
+      }
+      else  if(checkCounter() == true) {
+        function startTimer() {
+          $("td").toggleClass("cards")
+          console.log("startTimer ran")
+        }
+        setTimeout(startTimer, 1000);
 
+      }
+  })
+  })
+
+
+/* click counter start*/
 var turnsTaken = 0;
 
 // $(".cards").click(function(event){
@@ -18,10 +25,11 @@ var turnsTaken = 0;
 //     }
 // });
 
-function checkCounter(){
-  if(turnsTaken >= 2){
-    return false;
+function checkCounter() {
+  if (turnsTaken == 2) {
+    return true;
   }
   turnsTaken++;
-  return true;
+  return false;
 }
+/*click counter end*/
