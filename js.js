0eca8cdcde94a4462be877739618b361abcd5930
alea q <aleaq@aleas-AppleSmoothie.local>
2015-10-26 16:19:01 -0400

    adds class on click

--- js.js
+++ js.js
@@ -4,21 +4,31 @@ var boxopened = "";
 var imgopened = "";
 var num = 0;
 var moves = 0;
-var colorOptions = ["red","red","green","green","purple","purple","yellow","yellow"]
 
-$(function () {
-    $("tr.row td img").addClass('hidden');
-    addImg();
-    click();
-    check();
-    shuffle();
-});
+
+// $(function () {
+//     $("tr.row td img").addClass('hidden');
+//     addImg();
+//     click();
+//     check();
+//     shuffle();
+// });
 
 function randomFromTo(from, to) {
   return Math.floor(Math.random() * (to - from + 1) + from);
 }
-
-function shuffleCards(){
+/* randomizes the pick for the cards from the array */
+function addColorOrImg(){
+  var colorOptions = ["red","red","green","green","purple","purple","yellow","yellow"]
+  var optionUsed = [];
+/* splice adds and removes options from the array then puts them back in  */
+  $("td").each(function(){
+        var randomize =  Math.floor(Math.random() * colorOptions.length);
+        $(this).append('<style background="' + colorOptions[randomize] + '"/>');
+        if (optionUsed.indexOf(colorOptions[randomize] ) != -1) colorOptions.splice(randomize, 1);
+        else optionUsed.push(colorOptions[randomize] );
+        console.log(colorOptions)
+  });
 
 }
 
@@ -26,8 +36,20 @@ function clickCard(){
   var cardAmount = $("td").length
   $("td").click(function(){
     moves++
+
+    num++;
+    var cardAttr = $("td").attr("style")
+    $(this).addClass("selected")
   })
 }
+
+clickCard()
+
+
+function shuffleCards(){
+
+}
+
 
 
 // /* pick random color value */
