f7db5330947868c34678406ca066af8e5bdde6e5
alea q <aleaq@aleas-AppleSmoothie.local>
2015-10-26 15:46:09 -0400

    starting over

--- js.js
+++ js.js
@@ -1,67 +1,71 @@
-// $(".cards").click(function() {
-// if (clickAmount < 2) {
-//   $(".cards").css("background", "red")
-//         console.log("red ran")
-// event.preventDefault() /* blue is default*/
-//  }
-//       else if(checkCounter ==2) {
-//         function startTimer() {
-//           $(".cards").css("background","blue")
-//           console.log("startTimer ran")
-//   console.log(clickAmount)
-//         }setTimeout(startTimer, 1000);
+var lastCardSelected;
+var score = 0;
+var boxopened = "";
+var imgopened = "";
+var num = 0;
+var moves = 0;
+var colorOptions = ["red","red","green","green","purple","purple","yellow","yellow"]
+
+$(function () {
+    $("tr.row td img").addClass('hidden');
+    addImg();
+    click();
+    check();
+    shuffle();
+});
+
+function randomFromTo(from, to) {
+  return Math.floor(Math.random() * (to - from + 1) + from);
+}
+
+function shuffleCards(){
+
+}
+
+function clickCard(){
+  var cardAmount = $("td").length
+  $("td").click(function(){
+    moves++
+  })
+}
+
+
+// /* pick random color value */
 //
+// var colorOptions = ["red","red","green","green","purple","purple","yellow","yellow"]
+// var getCards = document.querySelector(".cards")
+// var getTd = document.querySelector("td")
+// var cardsFound = 0
+// var clickAmount = 0;
+//
+// function shuffleCards(){
+//   var allCards =
+//   var cardThis = $
+//   var cardArray = new array();
+// }
+//
+//
+//
+// for (var i = 0; i< getCards.length; i++)
+//   {
+//     function colorPicker(){
+//       var pickedCard = getCards[i]
+//       return randomColor = colorOptions[Math.floor(Math.random()* colorOptions.length)];
 //       }
 //
-// })
-
-var clickAmount = 0;
-function flip(){
-$(".cards").on("click", function() {
-  clickAmount = clickAmount + 1
-  $(this).css("background", "red")
-  if (clickAmount == 2) {
-    function startTimer() {
-      $(".cards").css("background", "blue")
-      clickAmount = 0
-    }
-    clearTimeout(startTimer)
-    setTimeout(startTimer, 1000);
-
-  }
-  console.log(clickAmount)
-
-});
-} flip()
-
-
-
-
-
-
-
-
-
-
-// function checkCounter() {
-// $(".cards").click(function(){
-//   if (clickAmount == 2) {
-//        return true;
-//
-//     } else {
-//       return false;
-//
 //     }
-//    console.log(clickAmount)
-// })
-// }
-
-// function checkCounter() {
-//   if (turnsAllowed == 2) {
-//     return true;
+//
+//
+// function flip(){
+// $(".cards").on("click", function() {
+//     clickAmount = clickAmount + 1
+//     $(this).css("background", colorPicker())
+//     var originalColor = $("td").css("background-color")
+//  if (clickAmount == 2) {
+//   var resetClick = clickAmount = 0
+//     setTimeout(function(){
+//       $("td").css("background",originalColor),resetClick
+//     },1000);
 //   }
-//   turnsAllowed++;
-//   console.log(turnsAllowed)
-//   return false;
-// }
-/*click counter end*/
+// });
+// } flip()
