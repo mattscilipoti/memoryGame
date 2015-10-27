0304429303f88170a1894d11bd3f0d57866e6c8a
alea q <aleaq@aleas-AppleSmoothie.local>
2015-10-27 10:44:55 -0400

    game is playable

--- js.js
+++ js.js
@@ -8,7 +8,7 @@ var moves = 0;
 $(function () {
   $("td").addClass("cardfront")
 addColorOrImg();
-
+// check()
 
 });
 
@@ -44,30 +44,40 @@ function clickCard(){
     num++;
     var card1 = $("td.selected").children().css("background-color")
     var card2 = $(this).children().css("background-color")
-    console.log(card1)
-    console.log(card2)
+    var click1 = $("td.selected").children()
+    var click2 = $(this).children()
+
       $(this).addClass("selected") /* need "this" to select indv cells*/
       $(this).removeClass("cardfront")
     var box = $("td").attr("id")
       if ($(this).children().css("display")=="none"){
           $(this).children().css("display", "block")
-        } else {$("td").css("display", "block")}
+        }
+        // else {$("td").css("display", "block")}
 
         if (num ==2) {
           num = 0
 
-          /*check if there is a match*/
-          if (card1==card2)
-          alert("you found a match!")
-          $("td").css("display","none")
 
         /*flips card after 1 second*/
-      setTimeout(function(){
+       if(card1 !== card2) {setTimeout(function(){
           $("td.selected").addClass("cardfront"),$("td").removeClass("selected")
           ,$("td").children().css("display", "none")
-        },1000)
+        },500)
+      }/*flip end*/
+
+      /*check if there is a match and remove if true*/
+      else if (card1==card2){
+      alert("you found a match!")
+      $("td.selected").remove()
+      $("td.selected").children().remove()
+      $(this).children().remove()
+      $(this).remove()
+      check() /*if no cards are left*/
+        }
 
       }/* if stmnt end*/
+
 
   })
 }
@@ -76,16 +86,10 @@ clickCard()
 
 
 
-function check(el) {
-
-    if ($(lastSelected).attr("td") == $(el).find("img").attr("td") && $(lastSelected).hasClass("visible")) {
-        score = 0 + 2;
-        alert("Congradulation! You scored!!" + " " + score + " Points");
+function check() {
+    if ($("td.cardfront").length==0){
+      alert("you won!")
     }
-
-    lastSelected = $(el).find("td");
-    clearAll();
-
 }
 
 
