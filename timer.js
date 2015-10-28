7ffcda6caf06fd417e7bae1164e68896cc7dbd76
alea q <aleaq@aleas-AppleSmoothie.local>
2015-10-28 09:06:02 -0400

    added styling changed button functionality

---
+++ timer.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+// function createTimer(){
+// var counter = 5;
+// var timer = null;
+// var counterBox = function(){ $(".numberbox").html("counter:"+ ' '+counter )}
+//
+// $("#button1").click(function(){
+// counterBox()
+//   })
+//   var timer = setInterval(function(){
+//    counter--;
+//     if (counter >= -1){
+//     counterBox.html("counter:"+ ' '+counter )
+//   }
+//     if (counter === 0)
+//       clearInterval(timer)
+//
+//     else {clearInterval(timer)}
+//
+//   }, 1000);
+//   };
+//
+//
+// createTimer()
+
+$("#button1").click(function(){
+  alert("oops this option doesn't work right now!")
+})
+
+});/*document end */
