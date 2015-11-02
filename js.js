c4f605c8828fae8f36178cea8f09d4752082108d
mattscilipoti <matt.scilipoti@ga.co>
2015-11-01 22:18:51 -0500

    format: proper indentation

--- js.js
+++ js.js
@@ -5,7 +5,7 @@ var moves = 0;
 
 $(function () {
   $("td").addClass("cardfront")
-addColorOrImg();
+  addColorOrImg();
 });
 
 /* randomizes the pick for the cards from the array */
@@ -13,13 +13,13 @@ function addColorOrImg(){
   var colorOptions = ["violet","grey","red","yellow"]
 
   var optionUsed = [];
-/* splice adds and removes options from the array then puts them back in  */
+  /* splice adds and removes options from the array then puts them back in  */
   $("td").each(function(){
-        var randomize =  Math.floor(Math.random() * colorOptions.length);
-        $(this).append('<td style = background:' + colorOptions[randomize] + ' id=cardback'+ '/>');
-        $("td#cardback").hide();
-        if (optionUsed.indexOf(colorOptions[randomize] ) != -1) colorOptions.splice(randomize, 1);
-        else optionUsed.push(colorOptions[randomize] );
+    var randomize =  Math.floor(Math.random() * colorOptions.length);
+    $(this).append('<td style = background:' + colorOptions[randomize] + ' id=cardback'+ '/>');
+    $("td#cardback").hide();
+    if (optionUsed.indexOf(colorOptions[randomize] ) != -1) colorOptions.splice(randomize, 1);
+    else optionUsed.push(colorOptions[randomize] );
   });
 
 }
@@ -37,26 +37,26 @@ function clickCard(){
     var click1 = $("td.selected").children()
     var click2 = $(this).children()
 
-        $(this).addClass("selected") /* need "this" to select indv cells*/
-        $(this).removeClass("cardfront")
+    $(this).addClass("selected") /* need "this" to select indv cells*/
+    $(this).removeClass("cardfront")
     var box = $("td").attr("id")
-      if ($(this).children().css("display")=="none"){
-          $(this).children().css("display", "block")
+    if ($(this).children().css("display")=="none"){
+      $(this).children().css("display", "block")
+    }
+
+    if (num ==2) {
+      num = 0
+
+      /*flips card after 1 second*/
+      if(card1 !== card2) {
+        score--;
+        $(".numberbox").html("score:"+ ' '+score )
+
+        /* let them know they kinda suck*/
+        if (score == -3){
+          alert("Seriously tho? try a little harder :-(")
         }
-
-        if (num ==2) {
-          num = 0
-
-        /*flips card after 1 second*/
-       if(card1 !== card2) {
-         score--;
-           $(".numberbox").html("score:"+ ' '+score )
-
-           /* let them know they kinda suck*/
-           if (score == -3){
-                alert("Seriously tho? try a little harder :-(")
-              }
-         setTimeout(function(){
+        setTimeout(function(){
           $("td.selected").addClass("cardfront"),$("td").removeClass("selected")
           ,$("td").children().css("display", "none")
         },500)
@@ -64,17 +64,17 @@ function clickCard(){
 
       /*check if there is a match and remove if true*/
       else if (card1==card2){
-          score++;
-          $(".numberbox").html("score:"+ ' '+score )
-      alert("you found a match!")
-      $("td.selected").remove()
-      $("td.selected").children().remove()
-      $(this).children().remove()
-      $(this).remove()
-      check() /*if no cards are left*/
-        }
+        score++;
+        $(".numberbox").html("score:"+ ' '+score )
+        alert("you found a match!")
+        $("td.selected").remove()
+        $("td.selected").children().remove()
+        $(this).children().remove()
+        $(this).remove()
+        check() /*if no cards are left*/
+      }
 
-      }/* if stmnt end*/
+    }/* if stmnt end*/
 
   })
 }
@@ -82,9 +82,9 @@ clickCard()
 
 /* check if any cards are left*/
 function check() {
-    if ($("td.cardfront").length==0){
-      alert("you won!")
-    }
+  if ($("td.cardfront").length==0){
+    alert("you won!")
+  }
 }
 
 /*show score on click and track it*/
@@ -92,9 +92,9 @@ function takeScore() {
   $("#button2").click(function(){
     $(".numberbox").html("score:"+ ' '+score )
 
-          })
+  })
 
-/* track the score*/
+  /* track the score*/
   // function changeScore(){
   //  if (card1==card2){
   //       score++;
@@ -110,6 +110,6 @@ function takeScore() {
   //      }
   //    }
 
- }
+}
 // changeScore()
- takeScore()
+takeScore()
